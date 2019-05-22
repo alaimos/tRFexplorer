125813332a6a56c244206e48c2f737ff92889aa9
Salvatore Alaimo <alaimos@gmail.com>
2019-05-22 23:10:23 +0200

    Interactive correlation graph

--- src/resources/bin/corr.graph.R
+++ src/resources/bin/corr.graph.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-packages      <- c("getopt", "ggplot2", "rjson", "ggpubr")
+packages      <- c("getopt", "ggplot2", "rjson", "ggpubr", "plotly", "htmlwidgets")
 not.installed <- setdiff(packages, rownames(installed.packages()))
 if (length(not.installed) > 0) {
   suppressMessages(suppressWarnings(try({
@@ -13,6 +13,8 @@ library(getopt, quietly = TRUE)
 library(ggplot2, quietly = TRUE)
 library(rjson, quietly = TRUE)
 library(ggpubr, quietly = TRUE)
+library(plotly, quietly = TRUE)
+library(htmlwidgets, quietly = TRUE)
 # get options, using the spec as defined by the enclosed list.
 # we read the options from the default: commandArgs(TRUE).
 spec <- matrix(c(
@@ -62,14 +64,31 @@ tryCatch({
   x       <- as.vector(dataset[opt$row.id,])
   y       <- as.vector(t(trf$RPM[opt$col.id,]))
   color   <- factor(trf$clinical[colnames(trf$RPM),]$tissue)
-  df      <- data.frame(x=x, y=y, Tissue=color, row.names = colnames(trf$RPM))
-  sp      <- ggscatter(df, x = "x", y = "y", color = "Tissue",
-                       add = "reg.line",
-                       add.params = list(color = "blue", fill = "lightgray"),
-                       xlab = opt$row.id, ylab = opt$col.id,
-                       conf.int = FALSE, cor.coef = TRUE, cor.method = opt$cor.method,
-                       cor.coeff.args = list(output.type="text"))
-  ggsave(opt$output.file, plot = sp, width = 20, height = 20, units = "cm", dpi = 600)
+  texts   <- paste0(opt$row.id, ": <b>", format(x, digits = 4), "</b><br />", 
+                    opt$col.id, ": <b>", format(y, digits = 4), "</b><br />",
+                    "Tissue: <b>", color, "</b>")
+  df      <- data.frame(x=x, y=y, Tissue=color, text=texts, row.names = colnames(trf$RPM))
+  ct      <- cor.test(x=df$x, y=df$y, method = opt$cor.method)
+  suppressWarnings({
+    sp      <- ggplot(data = df, aes(x=x, y=y)) +
+               geom_point(aes(text = text, color=Tissue)) + 
+               geom_smooth(method='lm') +
+               xlab(opt$row.id) +
+               ylab(opt$col.id) +
+               ggtitle(paste0("R = ", format(unname(ct$estimate), digits = 4)," , p = ", format(ct$p.value, digits = 4)))
+    pp <- ggplotly(sp, tooltip = c("text"))
+    saveWidget(as_widget(pp), file = opt$output.file, selfcontained = FALSE, 
+               libdir = paste0(dirname(opt$output.file), "/libs"),
+               title = paste0(opt$row.id, " vs ", opt$col.id))
+  })
+  # sp      <- ggscatter(df, x = "x", y = "y", color = "Tissue",
+  #                      add = "reg.line",
+  #                      add.params = list(color = "blue", fill = "lightgray"),
+  #                      xlab = opt$row.id, ylab = opt$col.id,
+  #                      conf.int = FALSE, cor.coef = FALSE, cor.method = opt$cor.method,
+  #                      cor.coeff.args = list(output.type="text"),
+  #                      main = paste0("R = ", format(unname(ct$estimate), digits = 4)," , p = ", format(ct$p.value, digits = 4)))
+  # ggsave(opt$output.file, plot = sp, width = 20, height = 20, units = "cm", dpi = 600)
 }, error=function (e) {
   cat(e$message);
   q(status = 105)
